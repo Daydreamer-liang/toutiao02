a6f0cfc1909dfa7c5fdc14ff8505d9300e8fd51e
zhuxiangliang <916447016@qq.com>
2020-03-01 12:12:52 +0800

    登录页完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '@/views/home'// 用一种简写的方式 @ 代表 /src
+import Login from '@/views/login'
+// import Login from '@/views/Login'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login,
+    name: 'login'
   }
+//   {
+//     path: '/about',
+//     name: 'About',
+//     // route level code-splitting按需加载
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+//   }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,55 @@
+<template>
+ <div class="login">
+     <!-- 表单 -->
+    <el-card class="login-card">
+        <div class="title">
+            <img src="../../assets/img/logo_index.png" alt="">
+        </div>
+        <el-from>
+            <el-from-item>
+                <el-input placeholder="请输入手机号"></el-input>
+            </el-from-item>
+            <el-from-item>
+                <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+                <el-button style="float:right" type="primary">点击发送</el-button>
+            </el-from-item>
+            <el-from-item>
+                <el-checkbox>我已经同意了，行了吧</el-checkbox>
+            </el-from-item>
+            <el-from-item>
+                <el-button  type="primary" style="width:100%">登录</el-button>
+            </el-from-item>
+        </el-from>
+   </el-card>
+    <!-- 表单 -->
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login{
+  background-image: url('../../assets/img/login_bg.jpg');
+  height: 100vh;//当前可视区域分成100份
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card{
+     width: 400px;
+     height: 300px;
+
+  }
+  .title{
+      text-align: center;
+      img{
+       height: 40px;
+    }
+  }
+}
+
+</style>>
