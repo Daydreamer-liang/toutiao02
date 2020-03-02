de4a1f6cf7b3d8d498d9b87459468608250692a6
zhuxiangliang <916447016@qq.com>
2020-03-02 11:06:56 +0800

    login 自动校验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,55 +1,78 @@
 <template>
- <div class="login">
-     <!-- 表单 -->
-    <el-card class="login-card">
-        <div class="title">
-            <img src="../../assets/img/logo_index.png" alt="">
-        </div>
-        <el-form>
-            <el-form-item>
-                <el-input placeholder="请输入手机号"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input style="width:60%" placeholder="请输入验证码"></el-input>
-                <el-button style="float:right" type="primary">点击发送</el-button>
-            </el-form-item>
-            <el-form-item>
-                <el-checkbox>我已经同意了，行了吧</el-checkbox>
-            </el-form-item>
-            <el-form-item>
-                <el-button  type="primary" style="width:100%">登录</el-button>
-            </el-form-item>
-        </el-form>
-   </el-card>
+  <div class="login">
     <!-- 表单 -->
- </div>
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form :model="loginform" :rules="loginrules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginform.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginform.code" style="width:60%" placeholder="请输入验证码"></el-input>
+          <el-button style="float:right" type="primary">点击发送</el-button>
+        </el-form-item>
+        <el-form-item prop="checkbox">
+          <el-checkbox v-model="loginform.checkbox">我已经同意了，行了吧</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <!-- 表单 -->
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginform: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checkbox: false // 同意
+      },
+      loginrules: {
+        mobile: [
+          // required 为true 为该字段必填
+          { required: true, message: '手机号不能为空' },
+          //   1为开头第二位是 3-9 后面9个数随机
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码格式不正确' }
+        ],
+        checkbox: [{
+          validator: function (rule, value, callback) {
+            value ? callback() : callback(new Error('您必须同意我们的霸王条款'))
+          }
+        }]
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.login{
-  background-image: url('../../assets/img/login_bg.jpg');
-  height: 100vh;//当前可视区域分成100份
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh; //当前可视区域分成100份
   background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
-  .login-card{
-     width: 400px;
-     height: 300px;
-
+  .login-card {
+    width: 400px;
+    height: 300px;
   }
-  .title{
-      text-align: center;
-      img{
-       height: 40px;
+  .title {
+    text-align: center;
+    img {
+      height: 40px;
     }
   }
 }
-
 </style>>
