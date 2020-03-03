9d5d4098dbecc97b2abcedfe73e0afe1656fa6fc
zhuxiangliang <916447016@qq.com>
2020-03-04 00:04:44 +0800

    导航守卫，没有token 将不允许访问主页

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/permission'// 引入权限模块
+// import '@/permission' // 引入权限模块
 import ElementUI from 'element-ui'// 引入
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import '@/styles/index.less'// 引入初始化样式
 import axios from 'axios'// 引入axios
-import Components from '@/components'// 组件全局注册
+import Components from '@/components'// 组件引入
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 配置注册
 Vue.prototype.$axios = axios// 将axios赋值给Vue对象的原型属性：全局
 Vue.config.productionTip = false
 Vue.use(ElementUI)// 全局注册
-Vue.use(Components)// 全局注册
+Vue.use(Components)// 组件全局注册
 new Vue({
   router,
   render: h => h(App)
---
+++ src/permission/index.js
@@ -0,0 +1,38 @@
+// 专门 处理路由权限
+
+import router from '@/router'
+// import router from '@/router'
+// 注册全局的权限守卫
+router.beforeEach(function (to, from, next) {
+  // 如果页面地址 以 home开头 就进行token判断
+  if (to.path.startsWith('/home')) {
+    // 你要去主页 看看你有钥匙吗
+    const token = window.localStorage.getItem('user-token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    //   不是去主页 就放过
+    next()
+  }
+})
+
+// router.beforeEach(function (to, from, next) {
+//   //   next() // 表示放过
+//   // 首先应该判断 你需要拦截哪些页面 如果页面的地址以 /home为开头, 就需要进行token的判断
+//   // 判断你要去的页面是不是以 /home为开头
+//   if (to.path.startsWith('/home')) {
+//     //   表示你要去主页相关的页面 我要看看你有没有钥匙 你没有钥匙我把你一脚踢回 登录页
+//     const token = window.localStorage.getItem('user-token')
+//     if (token) {
+//       next() // 放过
+//     } else {
+//       next('/login') // 回登录页 去拿钥匙
+//     }
+//   } else {
+//     //   如果我不是去主页相关
+//     next() // 直接放过
+//   }
+// })
